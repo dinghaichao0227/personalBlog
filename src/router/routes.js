04bb61c0bbfe28ddc742fce2d058e2e34f300a95
dinghaichao0227 <3083570060@qq.com>
2022-11-03 20:21:39 +0800

    修改router

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,7 @@ const routes = [
     path: "/",
     name: "home",
     component: HomeView,
-    redirect: "githomePage",
+    redirect: "homePage",
     children: [
       {
         path: "homePage",
